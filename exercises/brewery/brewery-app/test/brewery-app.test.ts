63c56ecd0761aac02fa913de2fbb14e42897df9a
John Hunter <john.hunter@apperio.com>
2023-02-24 18:30:00 +0000

    Update the test a little

--- exercises/brewery/brewery-app/test/brewery-app.test.ts
+++ exercises/brewery/brewery-app/test/brewery-app.test.ts
@@ -5,18 +5,28 @@ import type { BreweryApp } from '../src/brewery-app.js';
 import '../src/brewery-app.js';
 
 describe('BreweryApp', () => {
-  let element: BreweryApp;
-  beforeEach(async () => {
-    element = await fixture(html`<brewery-app></brewery-app>`);
+  it('renders a header', async () => {
+    const app: BreweryApp = await fixture(
+      html`<brewery-app><p>Some intro</p></brewery-app>`
+    );
+
+    const h1 = app.shadowRoot!.querySelector('h1')!;
+    expect(h1).to.exist;
+    expect(h1.textContent).to.equal('KewlBrews');
   });
 
-  it('renders a h1', () => {
-    const h1 = element.shadowRoot!.querySelector('h1')!;
-    expect(h1).to.exist;
-    expect(h1.textContent).to.equal('My app');
+  it('renders a the intro slot', async () => {
+    const app: BreweryApp = await fixture(
+      html`<brewery-app><p>Some intro</p></brewery-app>`
+    );
+
+    const intro = app.querySelector('.intro')!;
+    // This is not dom testing!! :o
+    expect(intro).not.to.exist;
   });
 
   it('passes the a11y audit', async () => {
-    await expect(element).shadowDom.to.be.accessible();
+    const app: BreweryApp = await fixture(html`<brewery-app></brewery-app>`);
+    await expect(app).shadowDom.to.be.accessible();
   });
 });
